1dad2fd8b8fd11b662926070c8a9b676c7b0eddd
Aleksey Zhurkin <web.zhurkin@gmail.com>
2024-12-24 10:03:57 +0300

    Initial Commit

---
+++ Homework-2-11-ContactList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework-2-11-ContactList
+//
+//  Created by Алексей Журкин on 24.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
